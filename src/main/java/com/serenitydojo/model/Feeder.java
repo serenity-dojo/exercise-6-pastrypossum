5d34c1c5fc1cc27771c778bed7923827413937fc
pastrypossum <j.j@j.com>
2024-05-05 10:34:34 +0100

    Updated tests

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -2,6 +2,33 @@ package com.serenitydojo.model;
 
 public class Feeder {
     public String feeds(String animal, boolean isPremium) {
-        return "Food";
+
+        if(animal.equals("Dog")) {
+//            if(isPremium)
+//                return "Deluxe Dog Food";
+//            else
+//                return "Dog Food";
+
+            return (isPremium) ? "Deluxe Dog Food" : "Dog Food";
+        }
+        else if(animal.equals("Hamster")){
+//            if(isPremium)
+//                return "Lettuce";
+//            else
+//                return "Cabbage";
+
+            return (isPremium) ? "Lettuce" : "Cabbage";
+        }
+        else if(animal.equals("Cat")) {
+//            if(isPremium)
+//                return "Salmon";
+//            else
+//                return "Tuna";
+
+            return (isPremium) ? "Salmon" : "Tuna";
+        }
+        else {
+            return "What we can find in the cupboards";
+        }
     }
 }
